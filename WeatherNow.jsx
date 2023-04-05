b800baa6b2b8259b300552a64ebd770a4d444dab
I8534 <122339515+I8534@users.noreply.github.com>
2023-04-05 20:25:48 +0900

    Update WeatherNow.jsx

--- WeatherNow.jsx
+++ WeatherNow.jsx
@@ -50,25 +50,35 @@ const WeatherNow = () => {
 
 
   let image_dust;
+  let txt_dust;
   if (pm10 > 150) {
     image_dust = "./teamproject-img-main/icon/finedust-red.png";
+    txt_dust ="매우 나쁨"
   } else if (pm10 > 80) {
     image_dust = "./teamproject-img-main/icon/finedust-orange.png";
+    txt_dust ="나쁨"
   } else if (pm10 > 30) {
     image_dust = "./teamproject-img-main/icon/finedust-green.png";
+    txt_dust ="보통"
   } else {
     image_dust = "./teamproject-img-main/icon/finedust-blue.png";
+    txt_dust ="좋음"
   }
 
   let image_dust2;
+  let txt_dust2;
   if (pm25 > 75) {
     image_dust2 = "./teamproject-img-main/icon/finedust-red.png";
+    txt_dust2 ="매우 나쁨"
   } else if (pm25 > 35) {
     image_dust2 = "./teamproject-img-main/icon/finedust-orange.png";
+    txt_dust2 ="나쁨"
   } else if (pm25 > 15) {
     image_dust2 = "./teamproject-img-main/icon/finedust-green.png";
+    txt_dust2 ="보통"
   } else {
     image_dust2 = "./teamproject-img-main/icon/finedust-blue.png";
+    txt_dust2 ="좋음"
   }
 
 
@@ -109,25 +119,26 @@ const WeatherNow = () => {
   } 
 
 
-
   return (
     <div>
       <div className='box'>
         <div className='small-box1'>
           <img className='weather-box' src={image_weath} alt="weather condition" />
-          <p>현재온도 : {temp}</p>
+          <p>{temp}</p>
           <p>{weath_condition}</p>
-          <p>{(temp-20).toFixed(2)}°</p>
-          <p>{temp+3}°</p>
-          <p>대전광역시</p>
+          <p style={{color : '#227cff'}}>{(temp-20).toFixed(2)}°</p>
+          <p style={{color : '#ff1515'}}>{temp+3}°</p>
+          <p style={{color : '#9b9b9b'}}>대전광역시</p>
         </div>
 
         <div className='small-box2'>
           <p>미세</p>
           <img className="dust-box" src={image_dust} alt="Air Pollution" />
+          <p style={ txt_dust == "매우 나쁨" ? { color:'red'} : ( txt_dust == "나쁨" ? {color : 'orange'} : ( txt_dust == "보통" ? {color : 'green'} : ( txt_dust == "좋음" ? {color : 'blue'} : {color : 'blue'} ) ) ) }>{txt_dust}</p>
           <p>초미세</p>
           <img className="dust-box2" src={image_dust2} alt="Air Pollution2" />
-          <p>대전광역시</p>
+          <p style={ txt_dust2 == "매우 나쁨" ? { color:'red'} : ( txt_dust2 == "나쁨" ? {color : 'orange'} : ( txt_dust2 == "보통" ? {color : 'green'} : ( txt_dust2 == "좋음" ? {color : 'blue'} : {color : 'blue'} ) ) ) }>{txt_dust2}</p>
+          <p style={{color : '#9b9b9b'}}>대전광역시</p>
         </div>
       </div>
     </div>
@@ -136,8 +147,3 @@ const WeatherNow = () => {
 
 export default WeatherNow;
 
-
-          {/* <p>PM10: {pm10}</p>
-          <p>PM2.5: {pm25}</p>
-          <p>온도: {temp-18}/{temp}℃</p>
-          <p>날씨: {weath}</p> */}
